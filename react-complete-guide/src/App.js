af25d4ed4cb2f6dde0637c23cdafc4ee23f6a127
LNPappas <LPappas@gmail.com>
2019-11-11 11:41:35 -0800

    update App

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hey! I'm a React App!!</h1>
-      <Person name='Max' age='28' />
-      <Person name='Jean' age='58' />
-      <Person name='Steph' age='32'>My hobbies: Hiking</Person>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age:28 },
+      { name: 'Jean', age:58 },
+      { name: 'Steph', age:32 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Clicked!');
+    this.setState({persons: [
+      { name: 'Maximillian', age:28 },
+      { name: 'Jean', age:58 },
+      { name: 'Stephanie', age:32 }
+    ]});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hey! I'm a React App!!</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>I like to hike!</Person>
+      </div>
+    );
+  }
 }
 
 export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const person = (props) => {
-    return <p> I'm {props.name} and I'm {props.age} years old.</p>
+    return (
+        <div>
+            <p> I'm {props.name}!</p>
+            <p> I'm {props.age} years old.</p>
+            <p>{props.children}</p>
+            <br/>
+        </div>
+    )
 };
 
 export default person;
