388dcf234662e5cb2aa3f6a0f2b0fdb2b70584de
LNPappas <LPappas@gmail.com>
2019-11-19 12:07:48 -0800

    update

---
+++ burger/src/containers/BuildControls/BuildControls.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './BuildControls.module.css'
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'cheese'},
+    {label: 'Meat', type: 'meat'},
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+
+    </div>
+);
+
+export default buildControls;
